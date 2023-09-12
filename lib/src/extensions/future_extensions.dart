72953bbb1022d8ac8cbdd1d6ed423392c9e92105
Ricardo Boss <contact@ricardoboss.de>
2023-09-12 17:15:17 +0200

    Fix issue with null as future result

--- lib/src/extensions/future_extensions.dart
+++ lib/src/extensions/future_extensions.dart
@@ -5,8 +5,10 @@ extension FutureExtension<T> on Future<T> {
     Widget Function(BuildContext context, T result) builder, {
     Widget Function(BuildContext context)? onLoading,
     Widget Function(
-            BuildContext context, Object? error, StackTrace? stackTrace)?
-        onError,
+      BuildContext context, [
+      Object? error,
+      StackTrace? stackTrace,
+    ])? onError,
     Key? key,
     bool eagerLoadingState = false,
   }) {
@@ -14,18 +16,18 @@ extension FutureExtension<T> on Future<T> {
       key: key,
       future: this,
       builder: (context, snapshot) {
-        if (eagerLoadingState &&
-            snapshot.connectionState == ConnectionState.waiting) {
-          return onLoading?.call(context) ?? const SizedBox.shrink();
-        }
-
         if (snapshot.hasError) {
           return onError?.call(context, snapshot.error, snapshot.stackTrace) ??
               const SizedBox.shrink();
         }
 
-        if (snapshot.hasData) {
-          return builder(context, snapshot.requireData);
+        if (eagerLoadingState &&
+            snapshot.connectionState == ConnectionState.waiting) {
+          return onLoading?.call(context) ?? const SizedBox.shrink();
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          return builder(context, snapshot.data as T);
         }
 
         return onLoading?.call(context) ?? const SizedBox.shrink();
